0c18493e4f0b64530301600b90d0e2b362f01f83
Mugivhi <anwanirilwele@gmail.com>
2022-07-20 16:28:35 +0200

    push

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React,{useState} from 'react';
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet,Image,ImageBackground, Text,View,Modal,Alert,Pressable } from 'react-native';
 
 export default function App() {
